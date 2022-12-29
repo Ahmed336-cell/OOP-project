230d411a4b373d08deeecfd8e0cc0a7dffd73318
Ahmed Maher <65620946+Ahmed336-cell@users.noreply.github.com>
2022-12-29 13:07:15 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "TennisCourt.cpp"
+#include "Padel.cpp"
+#include "BasketBall.cpp"
+#include "FootBall.cpp"
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    int choice;
+    int flage;
+    do {
+
+
+        cout<<"do you want to reserve other ground ? enter 1";
+        cin>>flage;
+    } while (flage  = 1);
+}
